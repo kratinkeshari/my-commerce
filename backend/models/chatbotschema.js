c66d66812791a80a411a4e8ac6a7fa665f1a352f
Pranjul Mani <pranjulmani010@gmail.com>
2025-06-08 23:31:12 +0530

    chatbot added

---
+++ backend/models/chatbotschema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const chatbotschema = mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+    },
+
+    PhoneNumber:{
+        type:Number,
+        require:true,
+    },
+
+    email:{
+        type:String,
+        require:true,
+    },
+
+    querystring:{
+        type:String,
+        require:true,
+    },
+});
+
+const Chatbotschema = mongoose.models.Chatbotschema || mongoose.model('Chatbotschema', chatbotschema);
+
+export default Chatbotschema;
